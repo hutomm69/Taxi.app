56a78933cf4865630e691119b4809b0f0feedb42
hutomm69 <yogahutomo60@students.amikom.ac.id>
2022-03-29 07:17:42 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi1 taxi = new Taxi1();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi1
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            if (OnDuty == true)
+            {
+                Console.WriteLine("DriverName : {0}", DriverName);
+                Console.WriteLine("OnDuty : Yes");
+                Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            }
+            else
+            {
+                Console.WriteLine("DriverName : {0}", DriverName);
+                Console.WriteLine("OnDuty : No");
+                Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            }
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("\n{0} sedang mengantar pwnumpang", DriverName);
+        }
+    }
+}
